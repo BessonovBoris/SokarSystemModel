33991471587a12f6493966f09877ae487fa03d52
Boris <borisimo.work@gmail.com>
2023-12-30 03:11:49 +0300

    add: боже, как делать поворот камеры, я уже ничего не понимаю, почему в gamedev так много матеши...

--- Render3D/Game1.cs
+++ Render3D/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,11 +7,13 @@ namespace Game1;
 
 public class Game1 : Game
 {
+    private const float CamSpeed = -0.001f;
+
+    private readonly Vector3 _camPos;
+    private Vector3 _camTar;
+
     private GraphicsDeviceManager _graphics;
     private SpriteBatch? _spriteBatch;
-
-    private Vector3 _camPos;
-    private Vector3 _camTar;
 
     private Matrix _projectionMatrix;
     private Matrix _viewMatrix;
@@ -21,15 +24,16 @@ public class Game1 : Game
 
     public Game1()
     {
-        _camPos = new Vector3(3, 0, 4);
-        _camTar = new Vector3(0, 0, 0);
+        _camPos = new Vector3(0, 0, 0);
+        _camTar = new Vector3(1f, 0, 1f);
 
         _graphics = new GraphicsDeviceManager(this);
         _graphics.IsFullScreen = false;
 
-        _graphics.PreferredBackBufferWidth = 1920;
-        _graphics.PreferredBackBufferHeight = 1080;
+        _graphics.PreferredBackBufferWidth = 864;
+        _graphics.PreferredBackBufferHeight = 486;
         _graphics.ApplyChanges();
+
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
     }
@@ -40,11 +44,11 @@ public class Game1 : Game
 
         _projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
             MathHelper.PiOver4,
-            (float)Window.ClientBounds.Width / (float)Window.ClientBounds.Height,
+            Window.ClientBounds.Width / (float)Window.ClientBounds.Height,
             1,
             100);
 
-        _worldMatrix = Matrix.CreateWorld(new Vector3(0f, 0f, 0f), new Vector3(0, 0, -1), Vector3.Up);
+        _worldMatrix = Matrix.CreateWorld(new Vector3(0f, 0f, 10f), new Vector3(0, 0, -1), Vector3.Up);
 
         base.Initialize();
     }
@@ -64,26 +68,76 @@ public class Game1 : Game
 
         if (Keyboard.GetState().IsKeyDown(Keys.Up))
         {
-            _worldMatrix *= Matrix.CreateRotationX(MathHelper.ToRadians(1));
+            // _worldMatrix *= Matrix.CreateRotationX(MathHelper.ToRadians(1));
+            var rotateMatrixY = new Matrix(
+                new Vector4((float)Math.Cos(CamSpeed), 0, -(float)Math.Sin(CamSpeed), 0),
+                new Vector4(0, 1, 0, 0),
+                new Vector4((float)Math.Sin(CamSpeed), 0, (float)Math.Cos(CamSpeed), 0),
+                new Vector4(0, 0, 0, 1));
+
+            _camTar.Y += rotateMatrixY.Determinant();
         }
 
         if (Keyboard.GetState().IsKeyDown(Keys.Down))
         {
-            _worldMatrix *= Matrix.CreateRotationX(-1 * MathHelper.ToRadians(1));
+            // _worldMatrix *= Matrix.CreateRotationX(-1 * MathHelper.ToRadians(1));
+            var rotateMatrixY = new Matrix(
+                new Vector4((float)Math.Cos(CamSpeed), 0, -(float)Math.Sin(CamSpeed), 0),
+                new Vector4(0, 1, 0, 0),
+                new Vector4((float)Math.Sin(CamSpeed), 0, (float)Math.Cos(CamSpeed), 0),
+                new Vector4(0, 0, 0, 1));
+
+            _camTar.Y -= rotateMatrixY.Determinant();
         }
 
         if (Keyboard.GetState().IsKeyDown(Keys.Left))
         {
-            _worldMatrix *= Matrix.CreateRotationY(MathHelper.ToRadians(1));
+            // _worldMatrix *= Matrix.CreateRotationY(-1 * MathHelper.ToRadians(1));
+            var rotateMatrixZ = new Matrix(
+                new Vector4((float)Math.Cos(CamSpeed), (float)Math.Sin(CamSpeed), 0, 0),
+                new Vector4(-(float)Math.Sin(CamSpeed), (float)Math.Cos(CamSpeed), 0, 0),
+                new Vector4(0, 0, 1, 0),
+                new Vector4(0, 0, 0, 1));
+
+            _camTar.Z -= rotateMatrixZ.Determinant();
         }
 
         if (Keyboard.GetState().IsKeyDown(Keys.Right))
         {
-            _worldMatrix *= Matrix.CreateRotationY(-1 * MathHelper.ToRadians(1));
+            // _worldMatrix *= Matrix.CreateRotationY(MathHelper.ToRadians(1));
+            var rotateMatrixZ = new Matrix(
+                new Vector4((float)Math.Cos(CamSpeed), (float)Math.Sin(CamSpeed), 0, 0),
+                new Vector4(-(float)Math.Sin(CamSpeed), (float)Math.Cos(CamSpeed), 0, 0),
+                new Vector4(0, 0, 1, 0),
+                new Vector4(0, 0, 0, 1));
+
+            _camTar.Z += rotateMatrixZ.Determinant();
+        }
+
+        if (Keyboard.GetState().IsKeyDown(Keys.W))
+        {
+            _worldMatrix *= Matrix.CreateTranslation(new Vector3(0, 0, -0.1f));
+        }
+
+        if (Keyboard.GetState().IsKeyDown(Keys.S))
+        {
+            _worldMatrix *= Matrix.CreateTranslation(new Vector3(0, 0, 0.1f));
+        }
+
+        if (Keyboard.GetState().IsKeyDown(Keys.A))
+        {
+            _worldMatrix *= Matrix.CreateTranslation(new Vector3(-0.1f, 0, 0));
+        }
+
+        if (Keyboard.GetState().IsKeyDown(Keys.D))
+        {
+            _worldMatrix *= Matrix.CreateTranslation(new Vector3(0.1f, 0, 0));
         }
 
         // _worldMatrix *= Matrix.CreateRotationX(MathHelper.ToRadians(1));
         // _worldMatrix *= Matrix.CreateRotationY(-1 * MathHelper.ToRadians(1));
+        _viewMatrix = Matrix.CreateLookAt(_camPos, _camTar, Vector3.Up);
+
         base.Update(gameTime);
     }
 
